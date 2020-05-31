29530b9d855bec476e71b01d04d863e1c0215bf2
Sean Oliver <snoliver@umich.edu>
2020-05-31 16:07:04 -0400

    removed target

--- guessing_game/target/debug/deps/rand_core-5af0865bc933f633.d
+++
@@ -1,7 +0,0 @@
-/mnt/c/Users/Sean/Documents/PersonalProjects/learn rust/guessing_game/target/debug/deps/rand_core-5af0865bc933f633.rmeta: /home/oliversno/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs
-
-/mnt/c/Users/Sean/Documents/PersonalProjects/learn rust/guessing_game/target/debug/deps/librand_core-5af0865bc933f633.rlib: /home/oliversno/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs
-
-/mnt/c/Users/Sean/Documents/PersonalProjects/learn rust/guessing_game/target/debug/deps/rand_core-5af0865bc933f633.d: /home/oliversno/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs
-
-/home/oliversno/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_core-0.3.1/src/lib.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/Sean/Documents/PersonalProjects/learn\ rust/guessing_game/target/debug/guessing_game: /mnt/c/Users/Sean/Documents/PersonalProjects/learn\ rust/guessing_game/src/main.rs
